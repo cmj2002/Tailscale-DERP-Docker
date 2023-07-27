bb438ba959afc151f92d5decb9e32f60f7b8cf8a
Cao Mingjun <me@caomingjun.com>
2023-07-27 10:25:11 +0000

    If it's not first start, don't use authkey

--- init.sh
+++ init.sh
@@ -11,13 +11,29 @@ chmod 600 /dev/net/tun
 # get comma separated list of arg $TAILSCALE_ARGS and convert to array
 IFS=',' read -r -a TAILSCALE_ARGS <<< "$TAILSCALE_ARGS"
 
+if [ -f /var/lib/tailscale/tailscaled.state ]; then
+    echo "Using existing state file. Skipping authkey."
+    INITED=true
+else
+    echo "No state file found. Using authkey."
+    INITED=false
+    if [ -z "$TAILSCALE_AUTHKEY" ]; then
+        echo "No authkey provided. Exiting."
+        exit 1
+    fi
+fi
+
 #Start tailscaled and connect to tailnet
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >> /dev/stdout &
 
 # sleep to wait for the daemon to start, default 2 seconds
 sleep "$TAILSCALE_SLEEP"
 
-/usr/bin/tailscale up --authkey="$TAILSCALE_AUTHKEY" "${TAILSCALE_ARGS[@]}" >> /dev/stdout &
+if [ "$INITED" = false ] ; then
+    /usr/bin/tailscale up --authkey="$TAILSCALE_AUTHKEY" "${TAILSCALE_ARGS[@]}" >> /dev/stdout &
+else
+    /usr/bin/tailscale up "${TAILSCALE_ARGS[@]}" >> /dev/stdout &
+fi
 
 # sleep to wait for the daemon to start, default 2 seconds
 sleep "$TAILSCALE_SLEEP"
